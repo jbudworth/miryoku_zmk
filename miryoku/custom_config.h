d87810dbac138191932d1e56b94ab41f38d3c2b0
jbudworth <j.budworth@gmail.com>
2024-03-11 18:52:21 +0800

    Added Ardux behaviour file

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// After the ARDUX dtsi has been included, set TAP and EXTRA to AZERTY enable the replacement layers
+#define MIRYOKU_TAP_AZERTY
+#define MIRYOKU_EXTRA_AZERTY
+#define MIRYOKU_CLIPBOARD_WIN
