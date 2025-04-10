9a521bc9ee3bc5e94d41f34a55c5162e21784138
jbudworth <j.budworth@gmail.com>
2025-04-10 12:01:40 +0800

    Update miryoku_mousekeys.h

--- miryoku/miryoku_mousekeys.h
+++ miryoku/miryoku_mousekeys.h
@@ -7,24 +7,6 @@
 #define U_BTN2 &mkp MB2
 #define U_BTN3 &mkp MB3
 
-/ {
-    macros {
-        U_NUDT: U_NUDT {
-            compatible = "zmk,behavior-macro";
-            #binding-cells = <0>;
-            tap-ms = <5>;
-            wait-ms = <100>;
-            bindings
-                = <&kp KP_NUM &kp KP_NUM>
-                , <&macro_pause_for_release>
-                , <&kp KP_NUM &kp KP_NUM>
-                ;
-        };
-    };
-};
-
-
-
 #define U_MS_D &kp KP_N2
 #define U_MS_L &kp KP_N4
 #define U_MS_R &kp KP_N6
